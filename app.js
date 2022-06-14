fea8a616cce4afe84b033ff32b219f307e399620
Josh Wallace <walliby84@gmail.com>
2022-06-13 21:26:04 -0400

    add route

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World sup!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World sup!");
+});
+
+app.get("/teapot", (req, res) => {
+  res.status(418);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
